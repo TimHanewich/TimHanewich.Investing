640cc7f1c5a127b1da4e0d9e14e8ff2d9522cdc7
Tim Hanewich <tahanewich@live.com>
2026-04-26 16:27:25 -0400

    Built out Calc perf

--- src/Simulation/Performance/PortfolioExtension.cs
+++ src/Simulation/Performance/PortfolioExtension.cs
@@ -1,12 +1,33 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Yahoo.Finance;
+using TimHanewich.Investing.Simulation;
 
 namespace TimHanewich.Investing.Simulation.Performance
 {
-    public static class Portfolio
+    public static class PortfolioExtension
     {
-        public static async Task<PortflioPerformance> CalculatePerformanceAsync()
+        public static async Task<PortflioPerformance> CalculatePerformanceAsync(this Portfolio portfolio)
         {
-            
+            Holding[] holdings = portfolio.Holdings();
+            List<HoldingPerformance> performances = new List<HoldingPerformance>();
+            foreach (Holding h in holdings)
+            {
+                Equity eq = Equity.Create(h.Symbol);
+                await eq.DownloadSummaryAsync();
+
+                HoldingPerformance hp = new HoldingPerformance();
+                hp.Symbol = h.Symbol;
+                hp.Quantity = h.Quantity;
+                hp.AverageCostBasis = h.AverageCostBasis;
+                hp.CurrentPrice = eq.Summary.Price;
+                performances.Add(hp);
+            }
+
+            PortflioPerformance pp = new PortflioPerformance();
+            pp.HoldingPerformances = performances.ToArray();
+            return pp;
         }
     }
 }
